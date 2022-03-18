0afdca169d1de4f739904f9e8b0c81ec22bd8c8f
YazatS123 <77256681+YazatS123@users.noreply.github.com>
2022-03-17 21:57:15 -0500

    Add files via upload

---
+++ c89-1/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBbtod7S-2ZCWUl4Uio_MFU6c7pppyb9vg",
+  authDomain: "storytelling-app-68754.firebaseapp.com",
+  databaseURL: "https://storytelling-app-68754-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-68754",
+  storageBucket: "storytelling-app-68754.appspot.com",
+  messagingSenderId: "324331758490",
+  appId: "1:324331758490:web:b3cc08fc28520b4c4bd023"
+};
